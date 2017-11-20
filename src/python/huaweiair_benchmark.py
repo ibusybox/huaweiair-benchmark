4203a20b222351186c74a1ffdf1628effdf523af
m00290915 <m00290915@huaweiobz.com>
2017-11-20 12:03:09 +0800

    修改为从portal端口登录访问系统

--- src/python/huaweiair_benchmark.py
+++ src/python/huaweiair_benchmark.py
@@ -9,6 +9,7 @@ import argparse
 import datetime
 import json
 import requests
+import urllib
 
 logger = logging.getLogger('huaweiair-benchmark')
 logger.setLevel(logging.INFO)
@@ -22,14 +23,20 @@ STAT_INTERVAL = 100
 SRC_MICROSERVICE = {'x-ces-src-microservice': 'gateway'}
 
 def call_query_orders(host, times=None, interval=None, userid="uid0@email.com"):
-    url = "%s/huaweiair/v1/orders" % host
+    url = "%s/orders/huaweiair/v1/orders" % host
     userid = "uid0@email.com" if userid is None else userid
-    times = sys.maxint if times is None or int(time) == 0 else int(times)
+    times = sys.maxint if times is None or int(times) == 0 else int(times)
     interval = 0 if interval is None else float(interval)
     playload = {'userId': userid}
     
     logger.info('start query orders with host: %s, times: %s, interval: %s, userid: %s', host, times, interval, userid)
-    headers = {'x-cse-context': json.dumps(SRC_MICROSERVICE)}
+
+    cookie = login(host)
+    if cookie is None:
+        logger.error("login failure")
+        return
+
+    headers = {'x-cse-context': json.dumps(SRC_MICROSERVICE), 'Cookie': cookie}
     n = 0
     success = 0
     failure = 0
@@ -42,6 +49,8 @@ def call_query_orders(host, times=None, interval=None, userid="uid0@email.com"):
                 failure = failure + 1
             logger.debug(
                 "query order by url %s at times %s, got response %s", url, n, response.status_code)
+        except requests.exceptions.ConnectionError:
+            logger.error('connection refused')
         except Exception as e:
             logger.error(
                 'query order by url %s at times %s with exception %s', url, n, e.message)
@@ -55,9 +64,9 @@ def call_query_orders(host, times=None, interval=None, userid="uid0@email.com"):
 
 
 def call_create_order(host, times=None, interval=None, userid="uid0@email.com"):
-    url = "%s/huaweiair/v1/orders" % host
+    url = "%s/orders/huaweiair/v1/orders" % host
     userid = "uid0@email.com" if userid is None else userid
-    times = sys.maxint if times is None or int(time) == 0 else int(times)
+    times = sys.maxint if times is None or int(times) == 0 else int(times)
     interval = 0 if interval is None else float(interval)
 
     toDate = datetime.datetime.now()
@@ -85,10 +94,16 @@ def call_create_order(host, times=None, interval=None, userid="uid0@email.com"):
 
     logger.info('start create order with host: %s, times: %s, interval: %s, userid: %s',
                 host, times, interval, userid)
+
+    cookie = login(host)
+    if cookie is None:
+        logger.error("login failure")
+        return
+
     n = 0
     success = 0
     failure = 0
-    headers = {'Content-Type': 'Application/json', 'x-cse-context': json.dumps(SRC_MICROSERVICE)}
+    headers = {'Content-Type': 'Application/json', 'x-cse-context': json.dumps(SRC_MICROSERVICE), 'Cookie': cookie}
     while n < times:
         try:
             response = requests.post(url, headers=headers, data=json.dumps(playload))
@@ -98,6 +113,8 @@ def call_create_order(host, times=None, interval=None, userid="uid0@email.com"):
                 failure = failure + 1
             logger.debug(
                 "create order by url %s at times %s, got response %s", url, n, response.status_code)
+        except requests.exceptions.ConnectionError:
+            logger.error('connection refused')
         except Exception as e:
             logger.error(
                 'create order by url %s at times %s with exception %s', url, n, e.message)
@@ -112,18 +129,24 @@ def call_create_order(host, times=None, interval=None, userid="uid0@email.com"):
 
 
 def call_pay_order(host, times=None, interval=None, userid="uid0@email.com"):
-    url = "%s/huaweiair/v1/orders/%s" % (host, uuid.uuid4().hex)
+    url = "%s/orders/huaweiair/v1/orders/%s" % (host, uuid.uuid4().hex)
     userid = "uid0@email.com" if userid is None else userid
-    times = sys.maxint if times is None or int(time) == 0 else int(times)
+    times = sys.maxint if times is None or int(times) == 0 else int(times)
     interval = 0 if interval is None else float(interval)
 
     playload = {'action': 1}
     logger.info('start pay order with host: %s, times: %s, interval: %s, userid: %s',
                 host, times, interval, userid)
+
+    cookie = login(host)
+    if cookie is None:
+        logger.error("login failure")
+        return
+
     n = 0
     success = 0
     failure = 0
-    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'x-cse-context': json.dumps(SRC_MICROSERVICE)}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'x-cse-context': json.dumps(SRC_MICROSERVICE), 'Cookie': cookie}
     while n < times:
         try:
             response = requests.put(
@@ -134,6 +157,8 @@ def call_pay_order(host, times=None, interval=None, userid="uid0@email.com"):
                 failure = failure + 1
             logger.debug(
                 "pay order by url %s at times %s, got response %s", url, n, response.status_code)
+        except requests.exceptions.ConnectionError:
+            logger.error('connection refused')
         except Exception as e:
             logger.error(
                 'pay order by url %s at times %s with exception %s', url, n, e.message)
@@ -147,15 +172,19 @@ def call_pay_order(host, times=None, interval=None, userid="uid0@email.com"):
 
 
 def call_delete_order(host, times=None, interval=None, userid="uid0@email.com"):
-    url = "%s/huaweiair/v1/orders/%s" % (host, uuid.uuid4().hex)
+    url = "%s/orders/huaweiair/v1/orders/%s" % (host, uuid.uuid4().hex)
     userid = "uid0@email.com" if userid is None else userid
-    times = sys.maxint if times is None or int(time) == 0 else int(times)
+    times = sys.maxint if times is None or int(times) == 0 else int(times)
     interval = 0 if interval is None else float(interval)
     
     logger.info('start delete order with host: %s, times: %s, interval: %s, userid: %s',
                 host, times, interval, userid)
-    
-    headers = {'x-cse-context': json.dumps(SRC_MICROSERVICE)}
+    cookie = login(host)
+    if cookie is None:
+        logger.error("login failure")
+        return
+
+    headers = {'x-cse-context': json.dumps(SRC_MICROSERVICE), 'Cookie': cookie}
     n = 0
     success = 0
     failure = 0
@@ -168,6 +197,8 @@ def call_delete_order(host, times=None, interval=None, userid="uid0@email.com"):
                 failure = failure + 1
             logger.debug(
                 "delete order by url %s at times %s, got response %s", url, n, response.status_code)
+        except requests.exceptions.ConnectionError:
+            logger.error('connection refused')
         except Exception as e:
             logger.error(
                 'delete order by url %s at times %s with exception %s', url, n, e.message)
@@ -189,6 +220,25 @@ def create_arg_parser():
                         help='call intervals, 1 means 1s, 0.1 means 100 ms')
     parser.add_argument('-u', '--userid', dest='userid', help='user id')
     return parser
+
+
+def login(host, userid="uid0@email.com", password="password"):
+    url = "%s/customers/rest/api/login" % (host)
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {'login': userid, 'password': password}
+    response = requests.post(url, data=data, headers=headers)
+    if response.status_code == 200:
+        # sessionid=53156a18-5a21-49d9-b844-009e1c2be227;path=/
+        set_cookie = response.headers.get('Set-Cookie')
+        logger.info('Set-Cookie in headers: %s', set_cookie)
+        sessiondid = set_cookie.split(';')[0].split('=')[1]
+        # sessionid=4aaa8aa2-19bf-4041-8169-6ef361661067; loggedinuser=uid0%40email.com
+        cookie = "sessionid=%s; loggedinuser=%s" % (sessiondid, urllib.quote(userid))
+        logger.info("cookie in headers: %s", cookie)
+        return cookie
+    else:
+        logger.error('login failure with user: %s, password: %s', userid, password)
+    return None
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
